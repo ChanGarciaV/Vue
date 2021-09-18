0145c0e97e6d667b229f4c8b65749e0baf8eeb34
ChanGarciaV <chayanne.garcia.valdez@gmail.com>
2021-09-17 20:22:05 -0500

    Se agregan 1 ejercicios de Vue

---
+++ demorouter/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <input type="button" value="Mensajes" @click="navega()" />
+     <h3>{{$route.params.demo}} </h3>
+  </div>
+</template>
+
+<script>
+//import { defineComponent } from '@vue/composition-api'
+
+export default {
+  methods:{
+    navega:function(){
+      this.$router.push("/messages"); // esta función hace que me rotee a la 
+
+    }
+  },
+}
+</script>
+
